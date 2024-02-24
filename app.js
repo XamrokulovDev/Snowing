43622bd43fbd5c6f62d543f8587ff97f63743e08
whoamihack1ng <whoamihack1ng@gmail.com>
2024-02-24 23:14:09 +0500

    loyiha tugatildi!

---
+++ app.js
@@ -0,0 +1,18 @@
+setInterval(CreateSnow,10)
+
+function CreateSnow(){
+    const snow = document.createElement("i");
+    snow.classList.add("fas");
+    snow.classList.add("fa-snowflake");
+
+    snow.style.left =Math.random()*window.innerWidth+"px";
+    snow.style.animationDuration =Math.random()*3+1+"s";
+    snow.style.opacity =Math.random();
+    snow.style.fontSize = Math.random()*10+10+"px";
+    document.body.append(snow);
+
+
+    setTimeout(()=>{
+        snow.remove();
+    },4000)
+}
